6f25b38063f66665b1d9c903f85650ca4bc43d0f
jireh <spencercebrian@gmail.com>
2023-11-03 23:57:51 +0800

    Deleted Unnecessary Files

--- cleanup/test.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
-get_ipython().run_cell_magic('bash', '', 'cd /content/gdrive/MyDrive\ngit clone https://github.com/WongKinYiu/yolov7.git\ncd yolov7\nwget https://raw.githubusercontent.com/WongKinYiu/yolov7/u5/requirements.txt\npip install -r requirements.txt\n')
-
